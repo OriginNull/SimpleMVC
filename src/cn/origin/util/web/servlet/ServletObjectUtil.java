fa9e3a9ad3e87772783d3614c80e1eb782e4daaa
OriginNull <297127732@qq.com>
2018-05-26 17:38:10 +0800

    增加工具类注释

--- src/cn/origin/util/web/servlet/ServletObjectUtil.java
+++ src/cn/origin/util/web/servlet/ServletObjectUtil.java
@@ -3,21 +3,48 @@ package cn.origin.util.web.servlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/**
+ * request、response对象的设置、获取与清除
+ */
 public class ServletObjectUtil {
 	public static final ThreadLocal<HttpServletRequest> REQUEST_THREADLOCAL = new ThreadLocal<HttpServletRequest>() ; 
 	public static final ThreadLocal<HttpServletResponse> RESPONSE_THREADLOCAL = new ThreadLocal<HttpServletResponse>() ;
+
+	/**
+	 * request 对象的设置
+	 * @param request
+	 */
 	public static void setRequest(HttpServletRequest request) {
 		REQUEST_THREADLOCAL.set(request);
 	}
+
+	/**
+	 * responser对象的设置
+	 * @param response
+	 */
 	public static void setResponse(HttpServletResponse response) {
 		RESPONSE_THREADLOCAL.set(response);
 	}
+
+	/**
+	 * request对象的获取
+	 * @return
+	 */
 	public static HttpServletRequest getRequest() {
 		return REQUEST_THREADLOCAL.get() ;
 	}
+
+	/**
+	 * response对象的获取
+	 * @return
+	 */
 	public static HttpServletResponse getResponse() {
 		return RESPONSE_THREADLOCAL.get() ;
 	}
+
+	/**
+	 * 清除request、response对象
+	 */
 	public static void clear() {
 		REQUEST_THREADLOCAL.remove(); 
 		RESPONSE_THREADLOCAL.remove(); 
